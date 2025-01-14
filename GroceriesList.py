5111f95663d8078e46b684fe7687e284ec8e45c7
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-13 23:18:11 -0330

    check row to see if it has whitespace if it doesn't append to our list

--- GroceriesList.py
+++ GroceriesList.py
@@ -8,7 +8,8 @@ def OpenGroceries():
   with open("groceries.txt",newline='') as file:
    for row in file:
     row = row.strip().replace("<h1>","").replace("<h1>","").replace("<h1>","").replace("</h1>","").replace("<ul>","").replace("<li>","").replace("</li>","").replace("</ul>","")
-    groceries.append(row)
+    if row:
+     groceries.append(row)
  except FileNotFoundError as e:
   print(f"File wasn't found check directory{e}")
  return groceries
