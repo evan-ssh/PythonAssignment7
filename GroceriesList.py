205830c033a74e6763277221ea8b0e025c0e333d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-13 23:07:05 -0330

    Open the groceries list

---
+++ GroceriesList.py
@@ -0,0 +1,14 @@
+def main():
+ groceries = OpenGroceries()
+ print(groceries)
+
+def OpenGroceries():
+ try:
+  groceries = []
+  with open("groceries.txt",newline='') as file:
+   for row in file:
+    print(row)
+ except FileNotFoundError as e:
+  print(f"File wasn't found check directory{e}")
+  return groceries
+main()
