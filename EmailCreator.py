64c3e3f92c297adcc4eaf6314476c7ddabfd5324
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-15 11:45:19 -0330

    Generate emails from file list

--- EmailCreator.py
+++ EmailCreator.py
@@ -1,8 +1,10 @@
 import csv
 def main():
  contact = OpenEmails()
- print(f"{contact[0]}")
-
+ command = int(input("Enter a command"))
+ if command == 1:
+  CreateEmails(contact)
+  
 def OpenEmails():
  try:
   contact = []
@@ -16,5 +18,14 @@ def OpenEmails():
  except FileNotFoundError as e:
   print(f"Could not find file check directory{e}")
  return contact
+
+def CreateEmails(contact):
+ for first_name,last_name,email in contact:
+  print(f"To:	     {email}")
+  print(f"From:	 noreply@deals.com")
+  print(f"Subject: Deals!\n")
+  print(f"Hi {first_name},\n")
+  print(f"We've got some great deals for you. Check our website!")
+
 
 main()
