7b2c3518dbdbd2d8764905470d67e3439defee61
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-15 10:27:26 -0330

    Add a player to the stats dictionary

--- GameStats.py
+++ GameStats.py
@@ -4,14 +4,25 @@ def main():
  if command == 1:
   SearchPlayer(players)
  elif command == 2:
-    ListPlayers()
+    ListPlayers(players)
+ elif command == 3:
+   AddPlayer(players)
 def SearchPlayer(players):
  for name,values in players.items():
   enter_name = input("Enter a player to Search for")
   if name == enter_name:
    print(f"{name},{values}")
    
-def ListPlayers():
- for i, name in enumerate(sorted(players.keys()))
+def ListPlayers(players):
+ for i, name in enumerate(sorted(players.keys()),start=1):
   print(f"{i}.{name}")
+
+def AddPlayer(players):
+   name = input("Enter a name for the new player")
+   wins = input("Enter the wins")
+   losses = input("Enter the losses")
+   ties =  input("Enter the ties")
+   players[name] = [wins,losses,ties]
+   print(f"Added stats for {name}")
+   ListPlayers(players)
 main()
