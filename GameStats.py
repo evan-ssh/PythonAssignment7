282ce1968342d2c9e0012c6511c03dde3f3570d0
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2025-01-14 22:35:20 -0330

    Search a player

--- GameStats.py
+++ GameStats.py
@@ -1,3 +1,13 @@
 def main():
  players = {"Elizabeth":{"Wins":"41","Losses":"22","Ties":"11"},"Jimmy":{"Wins":"41","Losses":"22","Ties":"11"},"Timmy":{"Wins":"41","Losses":"22","Ties":"11"}}
- 
+ command = int(input("Enter a command"))
+ if command == 1:
+  SearchPlayer(players)
+
+def SearchPlayer(players):
+ for name,values in players.items():
+  enter_name = input("Enter a player to Search for")
+  if name == enter_name:
+   print(f"{name},{values}")
+   
+main()
